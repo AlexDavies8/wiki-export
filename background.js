8152e17ee655be5e90c6d94dfb1c1f2d390eede7
Alex Davies <alex@frogtrade.com>
2024-04-25 19:42:32 +0100

    Removed useless files

--- background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.sender === 'content' && message.action === "load") {
-        chrome.pageAction.show(sender.tab.id);
-    }
-});
